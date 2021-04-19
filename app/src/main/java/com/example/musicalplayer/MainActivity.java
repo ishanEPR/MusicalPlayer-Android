a995384005d4f1e04b01aabb878971b95f8b6cd4
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-04-19 18:46:01 +0530

    updated musical Player app

--- app/src/main/java/com/example/musicalplayer/MainActivity.java
+++ app/src/main/java/com/example/musicalplayer/MainActivity.java
@@ -2,13 +2,116 @@ package com.example.musicalplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listviewsong);
+        runtimePermission();
+
+    }
+    public void runtimePermission(){
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        displaySongs();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    public ArrayList<File> findsong(File file)
+    {
+        ArrayList<File> arrayList=new ArrayList<>();
+        File[] files=file.listFiles();
+        for (File singlefile : files){
+            if (singlefile.isDirectory() && !singlefile.isHidden())
+            {
+                arrayList.addAll(findsong(singlefile));
+            }
+            else{
+                if (singlefile.getName().endsWith(".mp3")||singlefile.getName().endsWith(".wav"))
+                {
+                    arrayList.add(singlefile);
+                }
+            }
+        }
+        return  arrayList;
+    }
+    void displaySongs(){
+        final ArrayList<File> mysongs=findsong(Environment.getExternalStorageDirectory());
+        items=new String[mysongs.size()];
+        for (int i=0;i<mysongs.size();i++)
+        {
+            items[i]=mysongs.get(i).getName().toString().replace(".mp3","").replace(".wav","");
+        }
+//        ArrayAdapter<String> myAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+//        listView.setAdapter(myAdapter);
+
+        customAdapter customAdapter=new customAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+
+    class customAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View myView=getLayoutInflater().inflate(R.layout.list_item,null);
+            TextView textsong=myView.findViewById(R.id.txtsongname);
+            textsong.setSelected(true);
+            textsong.setText(items[position]);
+            return myView;
+        }
     }
 }
